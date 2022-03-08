8703fccea96648566ba43e2867228c719b181d95
emicif <emanuela.cifolelli@gmail.com>
2022-03-08 14:45:47 +0100

    console numeri da 1 a 100

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log('JS OK!');
+
+
+// Scrivi un programma che stampi in console i numeri da 1 a 100
+// multipli di 3 stampi “Fizz” 
+// multipli di 5 stampi Buzz.
+// multipli sia di 3 che di 5 stampi FizzBuzz
+// stili differenti CSS
+
+// Come faccio a sapere se un numero è divisibile per un altro? 
+// Abbiamo visto qualcosa di particolare che possiamo usare? 
+// Come creare nuovi elementi html e appenderli al container?
+
+
+// Stampi in console i numeri da 1 a 100
+
+for (let i = 1; i <= 100; i ++) {
+    console.log(i);
+}
