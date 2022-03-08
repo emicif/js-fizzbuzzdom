47aa27eabfedbb2b32a5ff8815ebd2e692b469ca
emicif <emanuela.cifolelli@gmail.com>
2022-03-08 15:56:58 +0100

    js

--- script.js
+++ script.js
@@ -17,29 +17,31 @@ console.log('JS OK!');
 const divNumber = document.querySelector("#number");
 let i = 1
 
+
 for (let i = 1; i <= 100; i ++) {
     const element = `<div class="num"> ${i} </div>`;
-    divNumber.innerHTML += element;
+    divNumber.innerHTML += element; 
     
     // multipli di 3 stampa “Fizz” 
     if(i % 3 == 0) { 
-    console.log("Fizz");  
-    }
+    console.log("Fizz");
+        }
     
-   
     // multipli di 5 stampa “Buzz”
-    if(i % 5 == 0) { 
+    else if(i % 5 == 0) { 
         console.log("Buzz");  
         }
     // multipli sia di 3 che di 5 stampa “FizzBuzz” 
-    if ( (i % 3 == 0) && (i % 5 == 0)) {
+    else if ( (i % 3 == 0) && (i % 5 == 0)) {
         console.log("FizzBuzz");
-       
-    } else {
-        console.log(i)
-    }
-    
+          
+        } else {
+            console.log(i)
+        }
+        
 }
+
+
 
 
 
