d5ee44a150826597060b96864d118faac0f77366
emicif <emanuela.cifolelli@gmail.com>
2022-03-08 14:51:59 +0100

    multipli - stampa differente

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ console.log('JS OK!');
 
 // Scrivi un programma che stampi in console i numeri da 1 a 100
 // multipli di 3 stampi “Fizz” 
-// multipli di 5 stampi Buzz.
-// multipli sia di 3 che di 5 stampi FizzBuzz
+// multipli di 5 stampi “Buzz”
+// multipli sia di 3 che di 5 stampi “FizzBuzz”
 // stili differenti CSS
 
 // Come faccio a sapere se un numero è divisibile per un altro? 
@@ -14,6 +14,24 @@ console.log('JS OK!');
 
 // Stampi in console i numeri da 1 a 100
 
+
 for (let i = 1; i <= 100; i ++) {
     console.log(i);
+    // multipli di 3 stampa “Fizz” 
+    if( i % 3 == 0 ) { 
+        console.log("Fizz");  
+        }
+    // multipli di 5 stampa “Buzz”
+    if( i % 5 == 0 ) { 
+        console.log("Buzz");  
+        }
+    // multipli sia di 3 che di 5 stampa “FizzBuzz” 
+    if ( (i % 3 == 0) && ( i % 5 == 0 )) {
+        console.log("FizzBuzz");
+    }
 }
+
+
+
+
+
